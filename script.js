68a3f17e363a30e773efc017948ee006b7303bf8
thegreatdestroyer <citizenerasedru@gmail.com>
2019-02-11 23:20:03 +0500

    working changediv

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function changeDiv(){
   var searching = document.getElementById("searching").value;  
   var div = document.createElement("div");
-  div.innerHTML = "<p>Hello World</p>";
-  document.getElementById("searched").appendChild(div);
+  div.innerHTML = searching;
+  document.getElementById("searched").innerHTML = searching;
 }
