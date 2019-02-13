52ffce1ab2eb296d69cd82e0e2abcd05c65d7785
thegreatdestroyer <citizenerasedru@gmail.com>
2019-02-14 00:41:45 +0500

    tryna make work

--- axiosfile.js
+++ axiosfile.js
@@ -1,13 +1,24 @@
-// Make a request for a user with a given ID
-axios.get('https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles=pizza')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
+$(document).ready(function(){
+  $("#searchbutton").click(function(){
+      $("#search").toggle();
   });
+  
+  $("#searchWiki").click(function(){
+      var q = document.getElementById("searchid").value;
+$('#results').html('');                
+      $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=" + q, function(data){
+          $('#results').append('<h2>Top 15 Wiki Search Results for "' + q + '"</h2>');
+          $.each(data.query.pages, function (i) {
+              $('#results').append("<p><a href='https://en.wikipedia.org/?curid=" + data.query.pages[i].pageid + 
+                  "' target='_blank'>" + data.query.pages[i].title + "</a></p>");
+          });
+          
+      });
+  });
+  
+  $("#clearSearch").click(function(){
+      document.getElementById("searchid").value = "";
+      document.getElementById("results").innerHTML = "";
+  });    
+  
+});
