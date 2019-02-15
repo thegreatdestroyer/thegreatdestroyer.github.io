c63f54b6ef7e24f83baf31f89dbcf3eeaee7ec28
thegreatdestroyer <citizenerasedru@gmail.com>
2019-02-15 20:43:29 +0500

    ru wiki

--- axiosfile.js
+++ axiosfile.js
@@ -9,7 +9,7 @@ $('#results').html('');
       $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=1&generator=search&origin=*&gsrsearch=" + q, function(data){
           $('#results').append('<h2>По этому запросу найдено на Википедии "' + q + '"</h2>');
           $.each(data.query.pages, function (i) {
-              $('#results').append("<p><a href='https://en.wikipedia.org/?curid=" + data.query.pages[i].pageid + 
+              $('#results').append("<p><a href='https://ru.wikipedia.org/?curid=" + data.query.pages[i].pageid + 
                   "' target='_blank'>" + data.query.pages[i].title + "</a></p>");
           });
           
