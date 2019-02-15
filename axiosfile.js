360a1abe76db17bdd57087a438b6e9bafbdf65fa
thegreatdestroyer <citizenerasedru@gmail.com>
2019-02-15 20:50:35 +0500

    bug fixes

--- axiosfile.js
+++ axiosfile.js
@@ -6,7 +6,7 @@
   $("#searchWiki").click(function(){
       var q = document.getElementById("searchid").value;
 $('#results').html('');                
-      $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=1&generator=search&origin=*&gsrsearch=" + q, function(data){
+      $.getJSON("https://ru.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=1&generator=search&origin=*&gsrsearch=" + q, function(data){
           $('#results').append('<h2>По этому запросу найдено на Википедии "' + q + '"</h2>');
           $.each(data.query.pages, function (i) {
               $('#results').append("<p><a href='https://ru.wikipedia.org/?curid=" + data.query.pages[i].pageid + 
